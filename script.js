f59eabcacc3977bcfd2d9aa926d678a739ba7e46
xKitsune47 <franek.cy@wp.pl>
2024-04-11 10:21:25 +0200

    commit

---
+++ script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+// console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByTagName('button'));
+// console.log(document.querySelectorAll('button'));
+// console.log(document.querySelector('button'));
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+    'We use cookies 🍪 <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+
+document
+    .querySelector('.btn--close-cookie')
+    .addEventListener('click', function (e) {
+        message.remove();
+    });
